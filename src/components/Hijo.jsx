98f9dc02954881707bdd3f86af60f9bb47be1a09
yersin77 <pruebacorreoselectronicosjyos@gmail.com>
2023-09-06 12:38:21 -0400

    modificaciones

--- src/components/Hijo.jsx
+++ src/components/Hijo.jsx
@@ -1,18 +1,40 @@
 import { json } from 'react-router-dom';
-import {contexto} from '../App'    
-import {useContext} from 'react';
+import { contexto } from '../App'
+import { useContext } from 'react';
 
-export function Hijo(){ 
+export function Hijo() {
 
-    const {isNombre} = useContext(contexto)
-    const datos = JSON.stringify(isNombre, null, 2) 
-    console.log(datos);
-    const datoJso = JSON.parse(datos)
-    console.log(datoJso);
-    return(
+
+    const funcionFetch = async ()=>{
+
+        const respuesta = await fetch('https://pokeapi.co/api/v2/pokemon/')
+        // console.log(respuesta);
+        const datos = await respuesta.json()
+        // console.log(datos.results);
+
+        const d1 = datos.results.map((item)=>{
+            item.name
+        })
+        
+        const d2 = await Promise.all(d1)
+        console.log(d2)
+
+        // console.log(datos.results);
+        // return datos.results
+
+        
+        
+
+    }
+
+    funcionFetch()
+    
+    
+    
+    return (
         <div>
             <h1>Hijo</h1>
-            <h1>{}</h1>
+            <h1>{ }</h1>
         </div>
-    )   
+    )
 }
