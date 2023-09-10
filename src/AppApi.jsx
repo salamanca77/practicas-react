28d1748b2f4809a6f72a458811c703d0bce5195c
jonn <joliversalamanca@gmail.com>
2023-09-10 07:25:05 -0400

    cambios

--- src/AppApi.jsx
+++ src/AppApi.jsx
@@ -1,18 +1,7 @@
-import { Routes, Route, Link } from "react-router-dom"
-import { Home } from '../src/page/Home'
-import  {Nosotros}  from '../src/page/Nosotros'
-import {Tienda} from '../src/page/Tienda'
-import {Contactos} from '../src/page/Contactos'
-import { Buscar } from "./page/Buscar"
-import { Detalles } from "./page/Detalles"
-import { ProtejerRuta } from "./page/ProtejerRuta"
-import { Login } from "./page/Login"
 import  {Formulario} from "./components/Formulario" 
-
 import React, { useState, createContext, useRef, useEffect } from "react"
 import { Table } from "./components/Tabla"
 import { helperHttp } from "./helper/helperHttp.js"
-
 
 export function AppApi(){
 
@@ -104,28 +93,5 @@ export function AppApi(){
         
          {db && <Table datos={db} deleteData={deleteData} setDataToEdit={setDataToEdit}/>}
         </>
-
-
-        // <>
-        // <nav>
-        //     <Link to="/">Home</Link>
-        //     <Link to="nosotros">Nosotros</Link>
-        //     <Link to="tienda">Tienda</Link>
-        //     <Link to="contacto">Contacto</Link>
-        //     <Link to="buscar" >Buscar</Link>
-        // </nav>
-        //     <Routes>
-        //         <Route path="/" element={<Home />} />
-        //         <Route path="/contacto" element={<ProtejerRuta><Contactos /></ProtejerRuta>} />
-        //         <Route path="/nosotros" element={<Nosotros />} />
-        //         <Route path="/tienda/:fruta" element={<Tienda />}>
-        //             <Route path="detalles" element={<Detalles />} />
-        //         </Route>
-        //         <Route path="/contactos" element={<Contactos />} />
-        //         <Route path="/buscar" element={<Buscar />} />
-        //         <Route path="/login" element={<Login />} />
-                
-        //     </Routes>
-        // </>
     )
 }
